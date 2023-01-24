affa12712dc862c02e2f1e659231cf2e368f49f5
Sven Sager <akira@narux.de>
2023-01-24 13:00:08 +0100

    Add default files for installation or packaging.

--- src/revpipyload/helper.py
+++ src/revpipyload/helper.py
@@ -2,7 +2,7 @@
 """Helperfunktionen fuer das gesamte RevPiPyLoad-System."""
 __author__ = "Sven Sager"
 __copyright__ = "Copyright (C) 2023 Sven Sager"
-__license__ = "GPLv3"
+__license__ = "GPLv2"
 
 import os
 from fcntl import ioctl
